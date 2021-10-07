2aa88fa371c8fb8104affaf00ca36a607d03e728
Jackie-Dou <kasach1477@mail.ru>
2021-10-07 21:03:54 +0300

    Добавьте файлы проекта.

---
+++ Lab2_SAiMMod/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace Lab2_SAiMMod
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.comboBoxTypes = new System.Windows.Forms.ComboBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelChange3 = new System.Windows.Forms.Label();
+            this.labelChange2 = new System.Windows.Forms.Label();
+            this.labelChange1 = new System.Windows.Forms.Label();
+            this.textBoxChange3 = new System.Windows.Forms.TextBox();
+            this.textBoxChange2 = new System.Windows.Forms.TextBox();
+            this.textBoxChange1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxM = new System.Windows.Forms.TextBox();
+            this.textBoxR0 = new System.Windows.Forms.TextBox();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxTypes
+            // 
+            this.comboBoxTypes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTypes.FormattingEnabled = true;
+            this.comboBoxTypes.Location = new System.Drawing.Point(12, 12);
+            this.comboBoxTypes.Name = "comboBoxTypes";
+            this.comboBoxTypes.Size = new System.Drawing.Size(218, 24);
+            this.comboBoxTypes.TabIndex = 0;
+            this.comboBoxTypes.SelectedValueChanged += new System.EventHandler(this.comboBoxTypes_SelectedValueChanged);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(90, 534);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(140, 27);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Сгенерировать";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelChange3);
+            this.panel1.Controls.Add(this.labelChange2);
+            this.panel1.Controls.Add(this.labelChange1);
+            this.panel1.Controls.Add(this.textBoxChange3);
+            this.panel1.Controls.Add(this.textBoxChange2);
+            this.panel1.Controls.Add(this.textBoxChange1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBoxM);
+            this.panel1.Controls.Add(this.textBoxR0);
+            this.panel1.Controls.Add(this.textBoxA);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 42);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(218, 246);
+            this.panel1.TabIndex = 2;
+            // 
+            // labelChange3
+            // 
+            this.labelChange3.AutoSize = true;
+            this.labelChange3.Location = new System.Drawing.Point(3, 192);
+            this.labelChange3.Name = "labelChange3";
+            this.labelChange3.Size = new System.Drawing.Size(20, 17);
+            this.labelChange3.TabIndex = 12;
+            this.labelChange3.Text = "f3";
+            // 
+            // labelChange2
+            // 
+            this.labelChange2.AutoSize = true;
+            this.labelChange2.Location = new System.Drawing.Point(3, 163);
+            this.labelChange2.Name = "labelChange2";
+            this.labelChange2.Size = new System.Drawing.Size(20, 17);
+            this.labelChange2.TabIndex = 11;
+            this.labelChange2.Text = "f2";
+            // 
+            // labelChange1
+            // 
+            this.labelChange1.AutoSize = true;
+            this.labelChange1.Location = new System.Drawing.Point(3, 134);
+            this.labelChange1.Name = "labelChange1";
+            this.labelChange1.Size = new System.Drawing.Size(20, 17);
+            this.labelChange1.TabIndex = 10;
+            this.labelChange1.Text = "f1";
+            // 
+            // textBoxChange3
+            // 
+            this.textBoxChange3.Location = new System.Drawing.Point(32, 192);
+            this.textBoxChange3.Name = "textBoxChange3";
+            this.textBoxChange3.Size = new System.Drawing.Size(100, 22);
+            this.textBoxChange3.TabIndex = 9;
+            // 
+            // textBoxChange2
+            // 
+            this.textBoxChange2.Location = new System.Drawing.Point(32, 163);
+            this.textBoxChange2.Name = "textBoxChange2";
+            this.textBoxChange2.Size = new System.Drawing.Size(100, 22);
+            this.textBoxChange2.TabIndex = 8;
+            // 
+            // textBoxChange1
+            // 
+            this.textBoxChange1.Location = new System.Drawing.Point(32, 134);
+            this.textBoxChange1.Name = "textBoxChange1";
+            this.textBoxChange1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxChange1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "m";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "R0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "a";
+            // 
+            // textBoxM
+            // 
+            this.textBoxM.Location = new System.Drawing.Point(32, 90);
+            this.textBoxM.Name = "textBoxM";
+            this.textBoxM.Size = new System.Drawing.Size(100, 22);
+            this.textBoxM.TabIndex = 3;
+            // 
+            // textBoxR0
+            // 
+            this.textBoxR0.Location = new System.Drawing.Point(32, 59);
+            this.textBoxR0.Name = "textBoxR0";
+            this.textBoxR0.Size = new System.Drawing.Size(100, 22);
+            this.textBoxR0.TabIndex = 2;
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(32, 31);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(100, 22);
+            this.textBoxA.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Входные данные";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(12, 294);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(218, 223);
+            this.panel2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 11);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 17);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Выходные данные";
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(251, 42);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1218, 475);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "f1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1513, 570);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.comboBoxTypes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxTypes;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelChange3;
+        private System.Windows.Forms.Label labelChange2;
+        private System.Windows.Forms.Label labelChange1;
+        private System.Windows.Forms.TextBox textBoxChange3;
+        private System.Windows.Forms.TextBox textBoxChange2;
+        private System.Windows.Forms.TextBox textBoxChange1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxM;
+        private System.Windows.Forms.TextBox textBoxR0;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Lab2_SAiMMod/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_SAiMMod
+{
+    public partial class Form1 : Form
+    {
+        private const int NUM_STEPS = 20;
+        private const int SIZE = 200000;
+        private const string EVEN_STR = "Равномерное распределение";
+        private const string GAUSS_STR = "Гауссовское распределение";
+        private const string EXP_STR = "Экспоненциальное распределение";
+        private const string GAMMA_STR = "Гамма-распределение";
+        private const string TRIANG_STR = "Треугольное распределение";
+        private const string SIMPSON_STR = "Распределение Симпсона";
+
+
+        /*
+                const double xMin = 0;
+                const double xMax = 1;
+                const double yMin = 0;
+                const double yMax = 0.1;
+                const double delta = 1 / (double)NUM_STEPS;*/
+
+        static List<double> numbersR = new List<double>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBoxTypes.Items.Add(EVEN_STR);
+            comboBoxTypes.Items.Add(GAUSS_STR);
+            comboBoxTypes.Items.Add(EXP_STR);
+            comboBoxTypes.Items.Add(GAMMA_STR);
+            comboBoxTypes.Items.Add(TRIANG_STR);
+            comboBoxTypes.Items.Add(SIMPSON_STR);
+        }
+
+        private void GetLemer(int a, int R0, int m)
+        {
+            double R = R0;
+            for (int i = 0; i < SIZE; i++)
+            {
+                double temp1 = (a * R) % m;
+                double temp2 = temp1 / m;
+                numbersR.Add(temp2);
+                R = temp1;
+            }
+        }
+
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            numbersR.Clear();
+
+            int a = 0, R0 = 0, m = 0;
+            try
+            {
+                a = Int32.Parse(textBoxA.Text);
+                R0 = Int32.Parse(textBoxR0.Text);
+                m = Int32.Parse(textBoxM.Text);
+                if (R0 >= (Math.Pow(2, SIZE) - 1) || a > m)
+                {
+                    throw new Exception("Ограничение на R");
+                }
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Некорректный ввод",
+                    "Сообщение");
+                return;
+            }
+            GetLemer(a, R0, m); 
+
+            string type;
+            type = comboBoxTypes.Text;
+
+            List<double> numbersX = new List<double>();
+            switch (type)
+            {
+                case EVEN_STR:
+                    numbersX = EvenDistribution(Convert.ToInt32(textBoxChange1.Text), Convert.ToDouble(textBoxChange2.Text), Convert.ToDouble(textBoxChange3.Text));
+                   // DrawEvaluationDiagram(numbersX, 5000, 10000, 0.06);
+                    break;
+                case GAUSS_STR:
+                    //numbersX = GaussDistribution();
+                    break;
+                case EXP_STR:
+                    //ExponentialDistribution();
+                    break;
+                case GAMMA_STR:
+                    break;
+                case TRIANG_STR:
+                    break;
+                case SIMPSON_STR:
+                    break;
+            }
+        }
+
+        private static List<double> EvenDistribution(int N = 130000, double a = 5000, double b = 10000)
+        {
+            if (N == 0) N = 130000;
+            if (a == 0) a = 5000;
+            if (b == 0) b = 10000;
+            var result = new List<double>();
+
+            for (int i = 0; i < N; i++)
+                result.Insert(i, a + (b - a) * numbersR.ElementAt(i));
+
+            return result;
+        }
+
+        private void comboBoxTypes_SelectedValueChanged(object sender, EventArgs e)
+        {
+            switch (comboBoxTypes.Text)
+            {
+                case EVEN_STR:
+                    labelChange1.Text = "N"; textBoxChange1.Text = "0";
+                    labelChange2.Text = "a"; textBoxChange2.Text = "0";
+                    labelChange3.Text = "b"; textBoxChange3.Text = "0";
+                    break;
+                case GAUSS_STR:
+                    break;
+                case EXP_STR:
+                    break;
+                case GAMMA_STR:
+                    break;
+                case TRIANG_STR:
+                    break;
+                case SIMPSON_STR:
+                    break;
+            }
+        }
+
+        /*        private void DrawEvaluationDiagram(List<double> xValues, double? minX = null, double? maxX = null, double? maxY = null)
+                {
+                    xValues.Sort();
+
+                    chart1.Series["Ci"].Points.Clear();
+                    chart1.Series["Xi"].Points.Clear();
+
+                    int[] countInIntervals = new int[NUM_STEPS + 1];
+                    int intervalNumber;
+                    int max = 0;
+
+                    double delta = (xValues.Last() - xValues.First()) / NUM_STEPS;
+
+                    foreach (double x in xValues)
+                    {
+                        intervalNumber = (int)Math.Truncate((x - xValues.First()) / delta) % 20;
+                        countInIntervals[intervalNumber]++;
+                    }
+
+                    foreach (int i in countInIntervals)
+                    {
+                        if (i > max)
+                        {
+                            max = i;
+                        }
+                    }
+
+                    chart1.ChartAreas[0].AxisY.Maximum = (maxY != null) ? maxY.Value : (1.5 * ((double)max / xValues.Count));
+                    chart1.ChartAreas[0].AxisY.Minimum = yMin;
+                    chart1.ChartAreas[0].AxisX.Maximum = (maxX != null) ? maxX.Value : xValues.Last();
+                    chart1.ChartAreas[0].AxisX.Minimum = (minX != null) ? minX.Value : xValues.First();
+
+                    for (int i = 0; i < countInIntervals.Length; i++)
+                    {
+                        chart1.Series["Ci"].Points.AddXY(xValues.First() + i * delta + delta / 2, (double)countInIntervals[i] / xValues.Count);
+                        Console.WriteLine((double)countInIntervals[i] / xValues.Count);
+                    }
+                }*/
+    }
+}
